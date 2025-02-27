c686010caaef12caa9536472abdaa80cc8d5b6eb
Xspixesdo <ebypogi123@gmail.com>
2025-02-27 13:04:46 -0800

    p dididues

--- Gambling.py
+++ Gambling.py
@@ -1,6 +1,26 @@
 MAX_BET = 1000
 MIN_BET = 0.50
 MAX_LINES = 6
+MIN_LINES = 1
+
+
+symbols = ["Cherry", "Bell", "Lemon", "Orange", "Star", "Skull"]
+payouts = {
+    "Cherry": 180,
+    "Bell": 100,
+    "Lemon": 90,
+    "Orange": 60,
+    "Star": 20,
+    "Skull": 2,
+}
+
+
+
+
+
+
+
+
 
 def Bet():
     while True:
@@ -18,6 +38,27 @@ def Bet():
 
 
 
-Bet()
+def Lines():
+    while True:
+        get_lines = input("How many lines would you like to play? (1-6):")
+        if get_lines.isdigit():
+            get_lines = int(get_lines)
+            if get_lines > MAX_LINES:
+                print("The maximum number of lines is 6")
+            elif get_lines < MIN_LINES:
+                print("The minimum number of lines is 1")
+            else:
+                return get_lines 
+        else:
+            print("Please enter a valid number.")
+
+
+def main():
+    get_bet = Bet()  
+    get_lines = Lines()  
+    print(f"Total bet: ${get_bet * get_lines}")
+    print("Good luck!")
+
+main()  
 
 
