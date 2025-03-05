ccf7558678246c21bb86b03c8abd9ca5d260c64f
Xspixesdo <ebypogi123@gmail.com>
2025-03-04 23:16:38 -0800

    les gooo

--- Gambling.py
+++ Gambling.py
@@ -15,8 +15,12 @@ Input_List_payouts = {
     "Skull": 2,
 }
 
+
+
+
 def Lines():
-    while True:
+    for line in range(1, MAX_LINES + 1):
+       while True:
         try:
             lines = int(input(f"Enter the number of lines to bet on ({MIN_LINES}-{MAX_LINES}): "))
             if MIN_LINES <= lines <= MAX_LINES:
@@ -25,6 +29,8 @@ def Lines():
                 print(f"Please enter a number between {MIN_LINES} and {MAX_LINES}.")
         except ValueError:
             print("Please enter a valid number.")
+       
+        break
 
 def Bet():
     while True:
@@ -36,6 +42,16 @@ def Bet():
                 print(f"Please enter an amount between {MIN_BET} and {MAX_BET}.")
         except ValueError:
             print("Please enter a valid number.")
+            break
+
+def balance(get_bet, total_wins):
+   balance = (get_bet - total_wins)
+   if balance > 0:
+       return balance
+    
+  
+                
+          
 
 def randomizer():
     return random.choice([True, False])
@@ -47,20 +63,37 @@ def wins():
     orange = random.choice(range(21))
     star = random.choice(range(11))
     skull = random.choice(range(16))
-    print(f"wins: {cherry} {bell} {lemon} {orange} {star} {skull}")
+    total_wins = cherry + bell + lemon + orange + star + skull
+    return total_wins
+
+def game():
+    get_bet = Bet() 
+    while True:
+        get_lines = Lines()
+        total_bet = get_bet * get_lines
+        
+        print(f"Total bet: ${total_bet}")
+        print("Good luck!")
+        print("Spinning...")
+        total_wins = wins()
+        print(f"Total wins: {total_wins}")
+        
+        if total_wins > 0.5:
+            print("Congratulations! You won!")
+            payout = get_bet * Input_List_payouts[symbols[total_wins % len(symbols)]] * get_lines
+            print(f"Total payout: ${payout}")
+           
+        continue
+        balance = get_bet - total_wins
+        print(f"Balance: ${get_bet-total_wins}")
+           
+        
+    
+       
 
 def main():
-    get_bet = Bet()
-    get_lines = Lines()
-    total_bet = get_bet * get_lines
-    print(f"Total bet: ${total_bet}")
-    print("Good luck!")
-    print("Spinning...")
-    print({wins()})
+
     
 
-    if randomizer():
-        wins()
-
-if __name__ == "__main__":
-    main()
+ if __name__ == "__main__":
+    game()
